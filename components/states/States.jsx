9c85777301e4919812ce148a32cd096d67bc8664
Julius Dames <jdames1@charlotte.edu>
2026-03-08 15:05:11 -0400

    Update States.jsx

--- components/states/States.jsx
+++ components/states/States.jsx
@@ -9,16 +9,41 @@ import './States.css';
 class States extends React.Component {
   constructor(props) {
     super(props);
-    console.log('window.models.states', window.models.states);
+    this.state = {
+      states: window.models.states(),
+      inputValue: ''
+    };
+    this.handleChangeBound = event => this.handleChange(event);
   }
-
-  render() {
+  
+  handleChange(event) {
+    this.setState({ inputValue: event.target.value });
+  }
+  
+  outOfBandJSX(filter) {
+    let optionJSX;
+    const listItems = this.state.states.filter(state => state.toLowerCase().includes(filter.toLowerCase()));
+    if (listItems.length === 0) {
+      optionJSX = <div>No matching states match filter criteria.</div>;
+    }
+    for (let i = 0; i < listItems.length; i++) {
+      listItems[i] = <li key={i}>{listItems[i]}</li>;
+    }
     return (
       <div>
-        Replace this with the code for Project 4, Problem 2
+          {optionJSX}
+          <ul>{listItems}</ul>
+        </div>
+    );
+  }
+  render() {
+    return (
+      <div className="container States">
+        <label htmlFor="stateFilterId">State Filter: </label>
+        <input id="stateFilterId" type="text" value={this.state.inputValue} onChange={this.handleChangeBound} />
+        {this.outOfBandJSX(this.state.inputValue)}
       </div>
     );
   }
 }
-
 export default States;
