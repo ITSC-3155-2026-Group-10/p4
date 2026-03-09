044a33bf5b21854e0da9bec0e4e41e9e737d57f0
macariallison30-blip <macariallison30@gmail.com>
2026-03-08 20:44:03 -0400

    Update p2.jsx

--- p2.jsx
+++ p2.jsx
@@ -1,9 +1,17 @@
+'use strict';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import States from './components/states/States';
+import Header from './components/header/header';
+import States from './components/states/states';
+
+ReactDOM.render(
+  <Header />,
+  document.getElementById('reactappHeader')
+);
 
 ReactDOM.render(
   <States />,
-  document.getElementById('reactapp'),
+  document.getElementById('reactapp')
 );
