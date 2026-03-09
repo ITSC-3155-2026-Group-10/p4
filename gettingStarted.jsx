43b2afb8854630cf01674746b1b647e7917ea724
macariallison30-blip <macariallison30@gmail.com>
2026-03-08 20:03:11 -0400

    Update GettingStartedApp component structure

--- gettingStarted.jsx
+++ gettingStarted.jsx
@@ -1,10 +1,24 @@
+'use strict';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import Header from './components/header/header';
+import Example from './components/example/example';
 import './styles/main.css';
 
-import Example from './components/example/Example';
+class GettingStartedApp extends React.Component {
+  render() {
+    return (
+      <div className="app-shell">
+        <Header />
+        <Example />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <Example />,
-  document.getElementById('reactapp'),
+  <GettingStartedApp />,
+  document.getElementById('reactapp')
 );
